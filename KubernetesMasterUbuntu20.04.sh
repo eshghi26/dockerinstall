b31816ba50ed360e173227ad34efd4abe47c19d7
eshghi26 <159582859+eshghi26@users.noreply.github.com>
2024-07-22 17:19:11 +0200

    Update KubernetesMasterUbuntu20.04.sh

--- KubernetesMasterUbuntu20.04.sh
+++ KubernetesMasterUbuntu20.04.sh
@@ -1,3 +1,4 @@
+echo "192.168.1.25 Master01" >> /etc/hosts #Replace your master node IP with this IP address
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
@@ -29,7 +30,7 @@ net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 sudo sysctl --system
-export OS="xUbuntu_20.04" #Replace your OS distributaion repo name with "xUbuntu20.04"
+export OS="xUbuntu_20.04" #Replace your OS distributaion repo name
 export VERSION="1.28"
 cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
@@ -44,8 +45,8 @@ sudo apt-get install cri-o cri-o-runc cri-tools -y
 sudo systemctl daemon-reload
 sudo systemctl enable crio --now
 sudo apt-get update
-sudo mkdir /etc/apt/keyrings
-sudo touch /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo mkdir /etc/apt/keyrings -y
+sudo touch /etc/apt/keyrings/kubernetes-apt-keyring.gpg -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list #Change url address based on you OS distribution
@@ -53,14 +54,13 @@ sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo apt-get install -y jq
-local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "ens192" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')" #Replace your interface name with "ens192"
+local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "ens192" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')" #Replace your interface name
 cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
 EOF
 export IPADDR="192.168.1.25" #Replace your master node IP with this IP address
 export NODENAME=$(hostname -s)
 export POD_CIDR="10.1.0.0/16"
-echo "192.168.1.25 Master01" >> /etc/hosts #Replace your master node IP with this IP address
 sudo kubeadm init --apiserver-advertise-address=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME --ignore-preflight-errors Swap
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
