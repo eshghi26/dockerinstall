66898a852d812c204d1e2ced61202ef0cf962e65
eshghi26 <159582859+eshghi26@users.noreply.github.com>
2024-07-22 17:21:21 +0200

    Update KubernetesWorkerUbuntu20.04.sh

--- KubernetesWorkerUbuntu20.04.sh
+++ KubernetesWorkerUbuntu20.04.sh
@@ -29,7 +29,7 @@ net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 sudo sysctl --system
-export OS="xUbuntu_20.04" #Replace your OS distributaion repo name with "xUbuntu20.04"
+export OS="xUbuntu_20.04" #Replace your OS distributaion repo name
 export VERSION="1.28"
 cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
@@ -43,14 +43,17 @@ sudo apt-get update
 sudo apt-get install cri-o cri-o-runc cri-tools -y
 sudo systemctl daemon-reload
 sudo systemctl enable crio --now
-sudo mkdir /etc/apt/keyrings
-sudo touch /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo mkdir /etc/apt/keyrings -y
+sudo touch /etc/apt/keyrings/kubernetes-apt-keyring.gpg -y
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo apt-get install -y jq
-local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "ens192" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')" #Replace your interface name with "ens192"
+local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "ens192" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')" #Replace your interface name
 cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
 EOF
